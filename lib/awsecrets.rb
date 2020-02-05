3ab3c5908b1ac62c4c6020a62d42a53a73909f40
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2020-02-04 23:13:01 -0300

    Rubocop fixes
    Created a new class method and used a guard instead of conditional
    execution.
    There is still other complaint (module with too many lines), but waiting
    another PR to be approved to merge with it and decide what to do next.

--- lib/awsecrets.rb
+++ lib/awsecrets.rb
@@ -140,18 +140,20 @@ module Awsecrets
     @credentials ||= Aws::SharedCredentials.new(profile_name: 'default') if AWSConfig['default'] && !@access_key_id
     @credentials ||= Aws::Credentials.new(@access_key_id, @secret_access_key, @session_token) if @access_key_id
     @credentials ||= Aws::InstanceProfileCredentials.new
-
-    if ENV.key?('DISABLE_AWS_CLIENT_CHECK') && !ENV['DISABLE_AWS_CLIENT_CHECK'].nil? && (ENV['DISABLE_AWS_CLIENT_CHECK'] == 'false')
-      begin
-        Aws::EC2::Client.new
-      rescue Aws::Errors::MissingRegionError
-        raise 'Missing region: use the region command line option or export region name to ENV[\'AWS_REGION\'] or use the awscli configuration'
-      rescue StandardError => e
-        raise "Oops, there is something wrong with AWS client configuration => #{e}"
-      end
-    end
-
+    self.validate_client
     Aws.config[:credentials] = @credentials
+  end
+
+  def self.validate_client
+    return unless ENV.key?('DISABLE_AWS_CLIENT_CHECK') && (ENV['DISABLE_AWS_CLIENT_CHECK'] == 'false')
+
+    begin
+      Aws::EC2::Client.new
+    rescue Aws::Errors::MissingRegionError
+      raise 'Missing region: use "region" command line option or export ENV[\'AWS_REGION\'] or awscli configure'
+    rescue StandardError => e
+      raise "Oops, there is something wrong with AWS client configuration => #{e}"
+    end
   end
 
   def self.generate_session_name
